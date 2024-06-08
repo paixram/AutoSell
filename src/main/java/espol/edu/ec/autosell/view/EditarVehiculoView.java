567568eff6ac162323dfcca0e824452ed81ea648
Josemiu <josemigueldelgadob@yahoo.com>
2024-06-07 23:56:55 -0500

    actualizacion

---
+++ src/main/java/espol/edu/ec/autosell/view/EditarVehiculoView.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espol.edu.ec.autosell.view;
+
+/**
+ *
+ * @author José Miguel
+ */
+public class EditarVehiculoView {
+    
+}
